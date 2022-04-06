c16e0a50def12a6a97e67309828844e712a37e42
Sarah <sarah@iMac-de-Sarah.local>
2022-04-06 14:27:51 +0200

    first commit

---
+++ script/hello.js
@@ -0,0 +1,10 @@
+(function(){
+  var date = new Date();
+  console.log(date);
+  document.getElementById('currentDate').value=date;
+
+  var products = [];
+  var product1 = {name: "herbe d'orge", description: "C'est génial et bon pour la santé !" , price: 20.00 };
+
+
+})(window);
